f1a08c625c5d5d7f75cb4bcef147cfa7d28a88ec
Arthur Grand <artygrand@gmail.com>
2022-08-01 20:23:11 +0900

    React setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        It works!
+      </header>
+    </div>
+  );
+}
+
+export default App;
